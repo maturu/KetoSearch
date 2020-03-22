1427f98f165c3f59800e23747b2eac0fb6833a9d
maturu <maturu.410@gmail.com>
2020-03-22 18:47:33 +0900

    chore: add meta description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,18 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'ケトサーチ - KetoSearch',
-      title: '糖質検索',
+      title: '糖質量などグラフで最も分かりやすい情報検索サービス',
       reverse: true,
       charset: 'utf-8',
-      description: 'ケトサーチは、食材や調味料の糖質・タンパク質・脂質などの情報を検索するためのサービスです。',
-      keywords: '糖質検索, ケトジェニック, ダイエット, 野菜 糖質, 糖質一覧, 糖質制限, ケトサーチ, ketosearch, ketogenic, diet, ローカーボ, ロカボ',
+      description: 
+        '
+          理想の体型を実現するため、正しく楽しく糖質制限。
+          ダイエット成功の秘訣/コツは、如何に期間を短く、効率よく減量できるかにかかっています。
+          そのため、ケトサーチでは、極端な糖質制限ダイエットを推奨しているWebサービスになります。
+          ケトサーチは、野菜などの食材や調味料の100gあたりの糖質・タンパク質・脂質などの情報を検索することができます。
+          また、糖質制限中に食べても良い食材が一覧でわかりやすいように、糖質が少ない順の食材ランキングなどを確認できます。
+        ',
+      keywords: '糖質検索, ケトジェニック, ダイエット, 野菜 糖質, 糖質 一覧 100g, 糖質制限, ケトサーチ, ketosearch, ketogenic, diet, ローカーボ, ロカボ',
       canonical: request.original_url,
       separator: '|',
       icon: [
