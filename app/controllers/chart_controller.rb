d82c53cf0c96e74cdf59a7b7829361fa4b768bc1
maturu <maturu.410@gmail.com>
2020-05-19 11:13:28 +0900

    fix: permit na column update

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -92,6 +92,6 @@ class ChartController < ApplicationController
 
   protected
     def food_params
-      params.require(:food).permit(:tag, :name, :gram, :calorie, :water, :protain, :lipid, :carbohydrate, :content, :fibtg, :reference, :url)
+      params.require(:food).permit(:tag, :name, :gram, :calorie, :water, :protain, :lipid, :carbohydrate, :content, :fibtg, :reference, :url, :na)
     end
 end
