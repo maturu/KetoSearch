5d2e6d02a0caac074e659d13222e235af367525b
maturu <maturu.410@gmail.com>
2020-05-31 22:48:02 +0900

    feat: qrcode with logo

--- app/helpers/chart_helper.rb
+++ app/helpers/chart_helper.rb
@@ -2,7 +2,12 @@ module ChartHelper
   require 'chunky_png'
 
   def qrcode_tag(url, options={})
-    qr = ::RQRCode::QRCode.new(url)
-    return ChunkyPNG::Image.from_datastream(qr.as_png.resize(300,300).to_datastream).to_data_url
+    qrcode = ::RQRCode::QRCode.new(url)
+    qr_image = qrcode.as_png()
+    logo = ChunkyPNG::Image.from_file('app/assets/images/qr_logo.png').resize(80,80)
+    height = 175 - (logo.dimension.height / 2).floor
+    width  = 175 - (logo.dimension.width  / 2).floor
+    qr_composed = qr_image.resize(350, 350).compose!(logo, width, height)
+    return ChunkyPNG::Image.from_datastream(qr_composed.to_datastream).to_data_url
   end
 end
