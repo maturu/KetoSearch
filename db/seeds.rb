512e665151572c3b43918dd07fae6a1b639dc16b
maturu <maturu.410@gmail.com>
2020-06-23 20:53:18 +0900

    chore: change seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ csv_text = File.read(Rails.root.join('lib', 'seeds', 'dataset.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each_with_index do |row, index|
   food = Food.find_by(name: row['NAME']).update!(
-    store_id: 1,
+    store_id: 2,
     price: 0
   )
 end
