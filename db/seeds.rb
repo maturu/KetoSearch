44b0f65f112f62298719b0a3a00be2ad3d690077
maturu <maturu.410@gmail.com>
2020-06-24 11:45:29 +0900

    chore: seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,19 @@ require 'csv'
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'dataset.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each_with_index do |row, index|
-  food = Food.find_by(name: row['NAME']).update!(
-    store_id: 2,
-    price: 0
+  food = Food.create!(
+    tag: row['TAG'],
+    name: row['NAME'],
+    calorie: row['CALORIE'],
+    water: row['WATER'],
+    protain: row['PROTAIN'],
+    lipid: row['LIPID'],
+    carbohydrate: row['CARBOHYDRATE'],
+    fibtg: row['FIBTG'],
+    na: row['NA'],
+    store_id: 32,
+    price: 0,
+    reference: "日本食品標準成分表2015年版（七訂）",
+    url: "https://www.mext.go.jp/a_menu/syokuhinseibun/1365297.htm"
   )
 end
